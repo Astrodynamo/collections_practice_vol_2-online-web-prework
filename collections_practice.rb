138f8aab042c5a3aed5ace3b2957f2784eabcb3a
Amanda King <amapickell@gmail.com>
2019-05-31 00:09:40 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,16 @@
-# your code goes here
+
+def begins_with_r(tools)
+  r_tools = 0
+
+   tools.each do |tool|
+    if tool.start_with?("r")
+      r_tools += 1
+    end
+  end
+
+   if r_tools == tools.length
+    return true
+  else
+    return false
+  end
+end
